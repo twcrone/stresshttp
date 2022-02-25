63afdd3866335be93b3b62e0b90927c0957ac713
Todd W Crone <twcrone@gmail.com>
2022-02-25 11:14:04 -0500

    Send simple request

--- stresshttp.go
+++ stresshttp.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello")
+	resp, err := http.Post("http://localhost:8080/submission", "text/plain",
+		bytes.NewBuffer([]byte("some,data")))
+	if err != nil {
+		print(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		print(err)
+	}
+	fmt.Println(string(body))
 }
