0b8df3aa82650eaf2f9bb0124bfec9d7dcb8c848
Todd W Crone <twcrone@gmail.com>
2022-02-25 11:58:49 -0500

    HTTP POST via go routines

--- stresshttp.go
+++ stresshttp.go
@@ -10,19 +10,14 @@ import (
 	"os"
 )
 
-func submissions() {
-	file, err := os.Open("test.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		submission(scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+func submissions(r int, data []string) {
+	for i, t := range data {
+		submission(t)
+		message := fmt.Sprintf("Routine %d - %d", r, i)
+		if r == 0 {
+			message = "***" + message + "***"
+		}
+		fmt.Println(message)
 	}
 }
 
@@ -42,6 +37,35 @@ func submission(text string) {
 	fmt.Println(string(body))
 }
 
+func readData() []string {
+	file, err := os.Open("test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return lines
+}
+
 func main() {
-	submissions()
+	data := readData()
+
+	go submissions(1, data)
+	go submissions(2, data)
+	go submissions(3, data)
+	go submissions(4, data)
+	go submissions(5, data)
+	go submissions(6, data)
+	go submissions(7, data)
+	go submissions(8, data)
+	go submissions(9, data)
+	submissions(0, data)
 }
